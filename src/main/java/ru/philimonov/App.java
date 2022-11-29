fdb0febbc6d2296f3d61dfd4e02620f16f4ec023
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-29 23:17:28 +0300

    add getAccount, createAccount, deleteAccount

--- src/main/java/ru/philimonov/App.java
+++ src/main/java/ru/philimonov/App.java
@@ -13,6 +13,10 @@ import java.util.Scanner;
 import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
 
 public class App {
+    private final static String URL = "jdbc:postgresql://localhost/postgres";
+    private final static String USER = "postgres";
+    private final static String PASSWORD = "password";
+
     public static void main(String[] args) throws SQLException {
         System.out.println("Aloha!!!");
         System.out.println("Press 1 for authorization, 2 for registration");
@@ -21,44 +25,120 @@ public class App {
 
         switch (request) {
             case 1:
-                String email = request("email: ");
-                String password = request("password: ");
-                try (Connection connection = DriverManager.getConnection(
-                        "jdbc:postgresql://localhost:5432/postgres",
-                        "postgres",
-                        "password");
-                     PreparedStatement preparedStatement = connection.prepareStatement("select * from service_user where email=? and password=?")) {
-                    preparedStatement.setString(1, email);
-                    preparedStatement.setString(2, password);
-
-                    try (ResultSet rs = preparedStatement.executeQuery()) {
-                        if (rs.next()) {
-                            System.out.println("Hello " + rs.getString("email"));
-                        } else {
-                            System.out.println("Access denied!!!");
-                        }
-                    }
+                int id = authentication();
+                System.out.println("press 1 - get list of accounts, 2 - create an account, 3 - delete an account");
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        getAccount(id);
+                        break;
+                    case 2:
+                        createAccount(id);
+                        break;
+                    case 3:
+                        deleteAccount(id);
+                        break;
                 }
-                break;
             case 2:
-                email = request("email: ");
-                password = request("password: ");
-                try (Connection connection = DriverManager.getConnection(
-                        "jdbc:postgresql://localhost:5432/postgres",
-                        "postgres",
-                        "password"
-                );
-                     PreparedStatement prepareStatement = connection.prepareStatement("insert into service_users (email, password) values (?,?)")) {
-                    prepareStatement.setString(1, email);
-                    prepareStatement.setString(2, md5Hex(password));
-                    int x = prepareStatement.executeUpdate();
-                    if (x == 0) {
-                        System.out.println("Fail!");
-                    } else {
-                        System.out.println("Success!");
-                    }
-                }
+                registration();
                 break;
+        }
+    }
+
+    public static Connection connect() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static int authentication() throws SQLException {
+        String email = request("email: ");
+        String password = request("password: ");
+        String sql = "select * from service_user where email=? and password=?";
+        int id = 0;
+        try (Connection connection = connect();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, email);
+            ps.setString(1, md5Hex(password));
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    id = rs.getInt("id");
+                    System.out.println("Hello " + rs.getString("email"));
+                } else {
+                    System.out.println("Access denied!");
+                }
+            }
+        }
+        return id;
+    }
+
+    public static void registration() throws SQLException {
+        String email = request("email: ");
+        String password = request("password: ");
+        String sql = "insert into service_user(email, password) values(?,?)";
+        try (Connection connection = connect();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, email);
+            ps.setString(2, md5Hex(password));
+            int number = ps.executeUpdate();
+            if (number == 0) {
+                System.out.println("Fail!!");
+            } else {
+                System.out.println("Success!");
+            }
+        }
+    }
+
+    public static void getAccount(int id) throws SQLException {
+        String sql = "select * from account where user_id = ?";
+        try (Connection connection = connect();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                System.out.println("Balance: " + rs.getString("name") +
+                        " is " + rs.getDouble("amount"));
+            }
+        }
+    }
+
+    public static void createAccount(int id) throws SQLException {
+        String account_title = request("account_title: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inpunt amount: ");
+        double amount = scanner.nextDouble();
+        String sql = "insert into account(name, amount, user_id) values(?,?,?)";
+        try (Connection connection = connect();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, account_title);
+            ps.setDouble(2, amount);
+            ps.setInt(3, id);
+            int x = ps.executeUpdate();
+            if (x == 0) {
+                System.out.println("Fail!");
+            } else {
+                System.out.println("Success!");
+            }
+        }
+    }
+
+    public static void deleteAccount(int id) throws SQLException {
+        String account_name = request("Enter account_title: ");
+        String sql = "delete from account where user_id = ? and name = ?";
+        try (Connection connection = connect();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            ps.setString(2, account_name);
+            int x = ps.executeUpdate();
+            if (x == 0) {
+                System.out.println("Fail!");
+            } else {
+                System.out.println("Success!");
+            }
         }
     }
 
